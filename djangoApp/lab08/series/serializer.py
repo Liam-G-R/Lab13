fc38926f5f3a470d7050239a246d6ae69cba7b0f
git config --global AlessandroADP <alessandro.davila@tecsup.edu.pe>
2025-06-11 14:22:23 -0500

    first commit

---
+++ djangoApp/lab08/series/serializer.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from .models import Serie, Category
+
+        
+class CategorySerializer(serializers.HyperlinkedModelSerializer):
+    
+    class Meta:
+        
+        model = Category
+        
+        fields = ('id' , 'description')
+
+
+class SerieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Serie
+        fields = ('id', 'name', 'description', 'img', 'category')
+        
+    def get_category_description(self, obj):
+        return obj.category.description
